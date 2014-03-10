12c0033b08a085ec0f2c0935b28d10bc042689a0
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-03-10 22:22:12 +0100

    LBSScriptFinished

--- kolab-3.1-test/setup.sh
+++ kolab-3.1-test/setup.sh
@@ -41,6 +41,5 @@ fi
 tar xjf $cachepath/phantomjs-1.9.2-linux-x86_64.tar.bz2
 cp phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/bin
 
-# close the pipes so that the calling python script can continue
-exec 1>&-
-exec 2>&-
+# tell the LBS that the calling python script can continue
+echo "LBSScriptFinished"
