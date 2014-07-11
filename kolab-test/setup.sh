45ad24138ffaf4984bdc7c2fb84bc7195b35d9fd
Timotheus Pokorra <tp@tbits.net>
2014-07-11 11:19:55 +0200

    install mail package for the tests

--- kolab-test/setup.sh
+++ kolab-test/setup.sh
@@ -8,7 +8,7 @@ fi
 # install some required packages
 if [ -f /etc/centos-release ]
 then
-  yum -y install python-setuptools wget which bzip2 || exit 1
+  yum -y install python-setuptools wget which bzip2 mail || exit 1
   cachepath=/var/cache/yum
 else
   if [ -f /etc/lsb-release ]
